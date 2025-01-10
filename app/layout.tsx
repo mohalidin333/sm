74dc37b8b2490626ccd3cd3dd332b28fa14fd2c8
mohalidin333 <mohalidinlidasan98@gmail.com>
2025-01-10 19:19:53 +0800

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
-
 import { Metadata } from "next";
 import "./globals.css";
 
-import { ThemeProvider } from "@/components/ui/theme-provider";
 import { GeistSans } from "geist/font/sans";
 
 export const metadata: Metadata = {
@@ -13,22 +11,12 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  
   return (
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon/favicon.ico" />
       </head>
-      <body className={`${GeistSans.className}`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
+      <body className={`${GeistSans.className}`}>{children}</body>
     </html>
   );
 }
